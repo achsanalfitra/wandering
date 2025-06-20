5b11fb21ad5569646fb9c4e607a645536de3c173
Alfitra Achsan <achsanalfitra@gmail.com>
2025-06-20 15:49:06 +0700

    feat(svm/test): create valid/bad data test cases for validate input

--- backend/internal/svm/handler_test.go
+++ backend/internal/svm/handler_test.go
@@ -1,13 +1,37 @@
 package svm
 
 import (
-	"log"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestValidateInput(t *testing.T) {
-	// create fake payload
-	// mock http connection for w and rq
-	// stub db
-	log.Println("Hello World")
+	// data
+	vd := []byte(`{
+		"real_vibes": ["loves_music", "loves_mountains"]
+	}`)
+
+	bd := []byte(`{
+		"real_vibes": ["loves_music", "haha"]
+	}`)
+
+	// call valid
+	rq := httptest.NewRequest("POST", "/", bytes.NewBuffer(vd))
+	w := httptest.NewRecorder()
+
+	HTest.ValidateInput(w, rq)
+	if w.Code != http.StatusAccepted {
+		t.Errorf("failed on valid data: %d", w.Code)
+	}
+
+	// call bad
+	rq = httptest.NewRequest("POST", "/", bytes.NewBuffer(bd))
+	w = httptest.NewRecorder()
+
+	HTest.ValidateInput(w, rq)
+	if w.Code != http.StatusNotFound {
+		t.Errorf("failed on bad data: %d", w.Code)
+	}
 }
